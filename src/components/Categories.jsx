3899eb1317c17153df75be48329fbab0d6b071d6
Serhii Shmyhelskyi <siro181a@gmai.com>
2023-02-14 13:13:45 +0200

    added blocks, render ValenukBlock, onClick for button

---
+++ src/components/Categories.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Categories() {
+  return (
+    <div className="categories">
+      <ul>
+        <li className="active">Всі</li>
+        <li>М'ясні</li>
+        <li>Вегетаріанські</li>
+        <li>Гриль</li>
+        <li>Гострі</li>
+        <li>Сирні</li>
+      </ul>
+    </div>
+  );
+}
